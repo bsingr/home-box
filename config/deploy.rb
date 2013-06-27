bed4fdc9c01b6f67623e0174a3908b5f0c2f4499
Jens Bissinger <mail@jens-bissinger.de>
2013-06-27 19:27:27 +0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,6 @@ set :deploy_via, :copy
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
 #   task :start do ; end
